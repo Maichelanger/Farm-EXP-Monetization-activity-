98b48967b703817424d282f72cdbaa11bcebd16d
Maichelanger <miguelangelcarrascobobo@hotmail.com>
2024-03-18 12:01:10 +0100

    Started Adverts Logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,12 +8,15 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject noSeedsPanel;
 
     private int seeds = 6;
+    private int seedsPlantedOnRound = 0;
 
     private string seedsTextString = "Semillas: ";
 
     private void Start()
     {
         seedsText.text = seedsTextString + seeds.ToString();
+
+        InitAds();
     }
 
     public void AddSeeds(int toAdd)
@@ -25,9 +28,16 @@ public class GameManager : MonoBehaviour
     public void SeedPlanted(Button pressedButton)
     {
         seeds--;
+        seedsPlantedOnRound++;
         seedsText.text = seedsTextString + seeds.ToString();
 
         pressedButton.interactable = false;
+
+        if (seedsPlantedOnRound >= 6)
+        {
+            seedsPlantedOnRound = 0;
+            ShowInsterticial();
+        }
 
         if (seeds <= 0)
         {
@@ -42,12 +52,29 @@ public class GameManager : MonoBehaviour
 
     //TODO: Add access to adverts
 
-    public void LoadAd()
+    private void InitAds()
+    {
+        //TODO
+        LoadInsterticial();
+        LoadRewarded();
+    }
+
+    private void LoadInsterticial()
     {
         //TODO
     }
 
-    public void ShowAd()
+    private void LoadRewarded()
+    {
+        //TODO
+    }
+
+    private void ShowInsterticial()
+    {
+        //TODO
+    }
+
+    public void ShowRewarded()
     {
         //TODO
     }
